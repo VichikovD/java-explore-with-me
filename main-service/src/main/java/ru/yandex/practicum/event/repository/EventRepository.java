1dabf1e7e6ab1133543cd42fbb01232df6d94239
VichikovD <rodderh@yandex.ru>
2024-02-06 03:31:43 +0300

    fix: error findAllFilteredAsUser

--- main-service/src/main/java/ru/yandex/practicum/event/repository/EventRepository.java
+++ main-service/src/main/java/ru/yandex/practicum/event/repository/EventRepository.java
@@ -3,7 +3,6 @@ package ru.yandex.practicum.event.repository;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
-import org.springframework.data.repository.query.Param;
 import ru.yandex.practicum.event.model.Event;
 import ru.yandex.practicum.event.model.PublishState;
 
@@ -35,8 +34,12 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             "OR cast(:rangeEnd AS timestamp) IS NULL " +
             "OR (e.eventDate BETWEEN :rangeStart AND :rangeEnd)" +
             ")")
-    List<Event> findAllFilteredAsAdmin(Collection<Long> users, Collection<PublishState> states, Collection<Long> categories,
-                                       LocalDateTime rangeStart, LocalDateTime rangeEnd, Pageable pageable);
+    List<Event> findAllFilteredAsAdmin(Collection<Long> users,
+                                       Collection<PublishState> states,
+                                       Collection<Long> categories,
+                                       LocalDateTime rangeStart,
+                                       LocalDateTime rangeEnd,
+                                       Pageable pageable);
 
     @Query(value = "SELECT * " +
             "FROM events AS e " +
@@ -59,11 +62,11 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             "GROUP BY e.event_id " +
             "HAVING COUNT(er.event_id) < e.participant_limit", nativeQuery = true)
     List<Event> findAllAvailableFilteredAsUser(
-            @Param("text") String text,
-            @Param("categories") Collection<Long> categories,
-            @Param("paid") Boolean paid,
-            @Param("rangeStart") LocalDateTime rangeStart,
-            @Param("rangeEnd") LocalDateTime rangeEnd,
+            String text,
+            Collection<Long> categories,
+            Boolean paid,
+            LocalDateTime rangeStart,
+            LocalDateTime rangeEnd,
             Pageable pageable);
 
     @Query(value = "SELECT * " +
@@ -83,68 +86,10 @@ public interface EventRepository extends JpaRepository<Event, Long> {
             ") " +
             "AND (e.state = 'PUBLISHED') ", nativeQuery = true)
     List<Event> findAllFilteredAsUser(
-            @Param("text") String text,
-            @Param("categories") Collection<Long> categories,
-            @Param("paid") Boolean paid,
-            @Param("rangeStart") LocalDateTime rangeStart,
-            @Param("rangeEnd") LocalDateTime rangeEnd,
+            String text,
+            Collection<Long> categories,
+            Boolean paid,
+            LocalDateTime rangeStart,
+            LocalDateTime rangeEnd,
             Pageable pageable);
-
-    /*@Query(value = "SELECT * " +
-            "FROM events AS e " +
-            "LEFT JOIN categories AS c ON e.category_id = c.category_id " +
-            "LEFT JOIN users AS u ON e.initiator_id = u.user_id " +
-            "LEFT JOIN event_requests AS er ON er.event_id = e.event_id " +
-            //        "JOIN FETCH e.location " +
-            "WHERE (LOWER(e.annotation) LIKE(LOWER(CONCAT('%', :text, '%'))) OR " +
-            "LOWER(e.description) LIKE(LOWER(CONCAT('%', :text, '%')))) AND " +
-            "(e.category_id IN :categories) AND " +
-            "(e.paid = :paid) AND " +
-            "(e.event_date > NOW()) AND " +
-            "(e.state = 'PUBLISHED') " +
-            "(er.status = 'CONFIRMED') " +
-            "GROUP BY e.event_id " +
-            "HAVING COUNT(er.event_id) < e.participant_limit", nativeQuery = true)
-    List<Event> findAllAvailableFilteredStartingFromToday(
-            @Param("text") String text,
-            @Param("categories") Collection<Long> categories,
-            @Param("paid") boolean paid,
-            Pageable pageable);
-
-    @Query(value = "SELECT * " +
-            "FROM events AS e " +
-            "LEFT JOIN categories AS c ON e.category_id = c.category_id " +
-            "LEFT JOIN users AS u ON e.initiator_id = u.user_id " +
-            //        "JOIN FETCH e.location " +
-            "WHERE (LOWER(e.annotation) LIKE(LOWER(CONCAT('%', :text, '%'))) OR " +
-            "LOWER(e.description) LIKE(LOWER(CONCAT('%', :text, '%')))) AND " +
-            "(e.category_id IN :categories) AND " +
-            "(e.paid = :paid) AND " +
-            "(e.event_date > NOW()) AND " +
-            "(e.state = 'PUBLISHED')", nativeQuery = true)
-    List<Event> findAllFilteredStartingFromToday(
-            @Param("text") String text,
-            @Param("categories") Collection<Long> categories,
-            @Param("paid") boolean paid,
-            Pageable pageable);
-
-    @Query(value = "SELECT * " +
-            "FROM events AS e " +
-            "LEFT JOIN categories AS c ON e.category_id = c.category_id " +
-            "LEFT JOIN users AS u ON e.initiator_id = u.user_id " +
-            //        "JOIN FETCH e.location " +
-            "WHERE (LOWER(e.annotation) LIKE(LOWER(CONCAT('%', :text, '%'))) OR " +
-            "LOWER(e.description) LIKE(LOWER(CONCAT('%', :text, '%')))) AND " +
-            "(e.category_id IN :categories) AND " +
-            "(e.paid = :paid) AND " +
-            "(e.event_date > :rangeStart) AND " +
-            "(e.event_date < :rangeEnd) AND " +
-            "(e.state = 'PUBLISHED') ", nativeQuery = true)
-    List<Event> findAllFiltered(
-            @Param("text") String text,
-            @Param("categories") Collection<Long> categories,
-            @Param("paid") boolean paid,
-            @Param("rangeStart") LocalDateTime rangeStart,
-            @Param("rangeEnd") LocalDateTime rangeEnd,
-            Pageable pageable);*/
 }
